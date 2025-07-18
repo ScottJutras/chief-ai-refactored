4bb6b01ce7a40d3afd11031d4cce7657e0db60f0
ScottJutras <scott@scottjutras.com>
2025-07-18 17:03:08 -0400

    Update index.js: ,router. preserve all functionality

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const webhookRouter = require('./routes/webhook');
 const dashboardRouter = require('./routes/dashboard');
 const deepDiveRouter = require('./handlers/deepDive');
+const { errorMiddleware } = require('./middleware/error');
 
 const app = express();
 
@@ -34,7 +35,7 @@ app.use((req, res) => {
   res.status(404).send('Not Found');
 });
 
-app.use(require('./middleware/error').errorMiddleware);
+app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

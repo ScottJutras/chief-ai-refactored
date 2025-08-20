5971fd1fe6a36dfc7844cba0db03ab996dcad39d
ScottJutras <scott@scottjutras.com>
2025-08-20 19:13:29 -0400

    update

--- utils/http.js
+++ utils/http.js
@@ -1,5 +1,14 @@
 // utils/http.js
 function ack(res) {
-  if (!res.headersSent) res.sendStatus(200);
+  if (!res.headersSent) {
+    // Return 200 with NO BODY (or an empty TwiML body).
+    // Either of these is fine; pick one and keep it consistent.
+
+    // Option 1: empty body
+    // res.status(200).end();
+
+    // Option 2: empty TwiML (recommended with Twilio)
+    res.status(200).type('text/xml').send('<Response/>');
+  }
 }
 module.exports = { ack };
